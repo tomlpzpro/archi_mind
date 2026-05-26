02f5c609ca860890ff3a761c745387a561dbdcb1
Tom LOPEZ <tomlpz.pro2@gmail.com>
2026-05-26 15:08:50 +0200

    Commit for merge

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_26_105419) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_26_105610) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -31,6 +31,14 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_26_105419) do
     t.string "title"
     t.datetime "updated_at", null: false
     t.index ["project_id"], name: "index_furnitures_on_project_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.bigint "chat_id", null: false
+    t.datetime "created_at", null: false
+    t.string "title"
+    t.datetime "updated_at", null: false
+    t.index ["chat_id"], name: "index_messages_on_chat_id"
   end
 
   create_table "projects", force: :cascade do |t|
@@ -57,5 +65,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_26_105419) do
   add_foreign_key "chats", "furnitures"
   add_foreign_key "chats", "projects"
   add_foreign_key "furnitures", "projects"
+  add_foreign_key "messages", "chats"
   add_foreign_key "projects", "users"
 end
