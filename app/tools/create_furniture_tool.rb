1b11503700b813880a5e9220cef25622ca28b38c
Tom LOPEZ <tomlpz.pro2@gmail.com>
2026-05-28 16:23:09 +0200

    create first tool furniture for description

---
+++ app/tools/create_furniture_tool.rb
@@ -0,0 +1,20 @@
+class CreateFurnitureTool < RubyLLM::Tool
+  description "Creer une description qui permettra de creer un objet sur demande d'un architecte d'interieur"
+  param :title, desc: "titre de la furniture"
+  param :description, desc: "description de la furniture"
+
+  def initialize(chat:)
+    @chat = chat
+  end
+
+  def execute(title:, description:)
+    @project = @chat.project
+    furniture = Furniture.create!(
+      title: title,
+      description: description,
+      project: @project,
+      chat: @chat
+    )
+    { project_id: furniture.project.id, chat_id: furniture.chat.id, description: furniture.description, title: furniture.title }
+  end
+end
--- config/initializers/ruby_llm.rb
+++ config/initializers/ruby_llm.rb
@@ -4,6 +4,6 @@
 
 RubyLLM.configure do |config|
   config.openai_api_key = ENV["OPENAI_API_KEY"]
-  config.openai_api_base = "https://models.inference.ai.azure.com"
+  # config.openai_api_base = "https://models.inference.ai.azure.com"
   # ... see RubyLLM configuration guide for other models
 end
